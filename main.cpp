10f0a786c14ee249dba1f6513b75c103f5ac373f
dawong15 <>
2021-04-08 23:27:10 +0000

    general file opening for the math and portguese classes

--- main.cpp
+++ main.cpp
@@ -1,5 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <iostream>
+#include <fstream>
+
+using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
+	ifstream inFSMath; //input file stream for the math class file
+	ifstream inFSPor; //input file stream for the portuguese class file
+
+
+	cout << "Attempting to open file 1 - student-mat.csv" << endl;
+
+	inFSMath.open("student-mat.csv");
+	if(!inFSMath.is_open()){
+		cout << "student-mat.csv could not be opened." << endl;
+	}
+
+	cout << "Attempting to open file 2 - student-por.csv" << endl;
+	inFSPor.open("student-por.csv");
+	if(!inFSPor.is_open()){
+		cout << "student-por.csv could not be opened." << endl;
+	}
+
+	//some kind of loop to iterate through one file, and append certain columns to a student(?)
+	//object, store each one in a vector
+
+	cout << "Closing files" << endl;
+	inFSMath.close();
+	inFSPor.close();
+
+	return 0;
 }
